72736ac766ee670122c27ae57d14fb9ba8c3404f
ghabrielferrarii <ghcferrari75@gmail.com>
2025-04-10 14:23:37 -0300

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Aluno 17 on 10/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
